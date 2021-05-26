0f5036b4d0e6cd2283dcdf5ba45780845b243c77
wjx <bigdata@itheima.com>
2021-05-26 21:40:13 +0800

    third commit

--- src/main/java/github_demo1.java
+++ src/main/java/github_demo1.java
@@ -2,5 +2,6 @@ public class github_demo1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
