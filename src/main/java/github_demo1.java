f02e5df575de47440a7267d0ab42212b9f726b65
wjx <bigdata@itheima.com>
2021-05-26 22:12:43 +0800

    push commit

--- src/main/java/github_demo1.java
+++ src/main/java/github_demo1.java
@@ -6,5 +6,6 @@ public class github_demo1 {
         System.out.println("hello git3");
         System.out.println("master test");
         System.out.println("hello_fix test");
+        System.out.println("push test");
     }
 }
